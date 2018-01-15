5515c7a9f3bd6f7f9d4d56eaa1caa5eb7f0492c9
adam <adstrzempa@gmail.com>
2018-01-15 11:25:30 +0100

    connect testrpc

--- test/TestExpertyToken.js
+++ test/TestExpertyToken.js
@@ -1,29 +1,7 @@
 const ExpertyToken = artifacts.require('./ExpertyToken');
 
-contract('ExpertyToken', accounts => {
-  let instance = null;
-  beforeEach(async () => instance = await ExpertyToken.new());
-  afterEach(() => instance = null);
-
-  it('should deposit 1000 coins in the first account', async () => {
-    const balanceBeforeDeposit = await instance.balanceOf.then(balance => balance);
-    await instance.deposit({ value: 1000 });
-    const balanceAfterDeposit = await instance.balanceOf.then(balance => balance);
-    assert.equal(balanceAfterDeposit, balanceBeforeDeposit + 1000);
-  });
-
-  it('should withdraw the requested amount', async () => {
-    await instance.deposit({ value: 1000 });
-    await instance.withdraw(500);
-    const balanceAfterWithdrawal = await instance.getBalance.balanceOf.then(balance => balance);
-    assert.equal(balanceAfterWithdrawal, 500)
-  });
-
-  it('should not allow withdraw more than deposited value', async () => {
-    await instance.deposit({ value: 500 });
-    await instance
-      .withdraw(1000)
-      .then(tx => { throw new Error() })
-      .catch(e => assert(e instanceof Error))
+contract('ExpertyToken', function(accounts) {
+  it("should put 10000 ExpertyToken in the first account", function() {
+    ExpertyToken.addContribution(accounts[0], 11).then(() => { return console.log('True')})
   });
 });
