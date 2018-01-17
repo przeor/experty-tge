e7540d7099b0ed4b62f17ace6e7e4dd936ac4084
hstemplewski <huberts@reactpoland.com>
2018-01-17 14:04:59 +0100

    change migration, test

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer, network) {
-  if (network === "development") {
-    try {
-      // try to unlock account for deploying the contracts
-      web3.personal.unlockAccount(web3.eth.accounts[0], "");
-    } catch(e) {}
-  }
+module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
--- test/TestExpertyToken.js
+++ test/TestExpertyToken.js
@@ -4,5 +4,6 @@ contract('ExpertyToken', accounts => {
   const instance = ExpertyToken.new();
   it("should put 1000 ExpertyToken in the first account", async () => {
     console.log("1");
+    instance.balanceOf.call()
   });
 });
