9c415c13e75150dea03b382e473bd6f0d6d6f5ef
hstemplewski <huberts@reactpoland.com>
2018-01-17 17:25:20 +0100

    test add contribution

--- test/TestExpertyToken.js
+++ test/TestExpertyToken.js
@@ -1,11 +1,25 @@
 const ExpertyToken = artifacts.require('./contracts/ExpertyToken');
+const web3 = global.web3;
 
 contract('ExpertyToken', accounts => {
-  let instance = null;
-  beforeEach(async () => instance = await ExpertyToken.new());
-  afterEach(() => instance = null);
-  it("should put 1000 ExpertyToken in the first account", async () => {
-    console.log("1");
-    instance.balanceOf.call()
+  it("should test add contribution", async () => {
+    //TODO: fix it.
+    const token = await ExpertyToken.new();
+    const balanceBefore = await getBalance(accounts[0]);
+    await token.addContribution.call(accounts[0], 1000);
+    const balanceAfter = await getBalance(accounts[0]);
+    assert(balanceAfter.eq(balanceBefore.add(1000)), 'Not add contribution');
   });
 });
+
+const getBalance = async (account) => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBalance(account, (error, balance) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(balance);
+      }
+    })
+  })
+};
