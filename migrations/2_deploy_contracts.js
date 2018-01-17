833e91eb0eea32f8d9b005b4ec4d8924eaf7323a
hstemplewski <huberts@reactpoland.com>
2018-01-17 16:42:12 +0100

    test configured

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-const ERC223Token = artifacts.require('./ERC223Token.sol');
 const ExpertyToken = artifacts.require("./ExpertyToken.sol");
+const MultisigExpertyEthControl = artifacts.require('./MultisigExpertyEthControl.sol');
+const MultisigExpertyLockedControl = artifacts.require('./MultisigExpertyLockedControl.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC223Token);
-  deployer.link(ERC223Token, ExpertyToken);
   deployer.deploy(ExpertyToken);
+  deployer.link(ExpertyToken, MultisigExpertyEthControl);
+  deployer.link(ExpertyToken, MultisigExpertyLockedControl);
+  deployer.deploy(MultisigExpertyEthControl);
+  deployer.deploy(MultisigExpertyLockedControl);
 };
