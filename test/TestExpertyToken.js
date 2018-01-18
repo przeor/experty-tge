f15baf4b9db86b8de1ed552690a47a253f0aa7d3
hstemplewski <huberts@reactpoland.com>
2018-01-18 09:56:56 +0100

    Add partner allocation test

--- test/TestExpertyToken.js
+++ test/TestExpertyToken.js
@@ -1,23 +1,33 @@
 const ExpertyToken = artifacts.require('ExpertyToken');
 const web3 = ExpertyToken.web3;
+// positions of field from lockedContributions struct
+// struct lockedContribution {
+//   uint exyPerPeriod;
+//   uint8 periods;
+//   uint8 claimedPeriods;
+//   uint8 periodDuration;
+// }
+const EXY_PER_PERIOD_FIELD = 0;
+const PERIOD_FIELD = 1;
+const CLAIMED_PERIODS_FIELD = 2;
+const PERIOD_DURAITON_FIELD = 3;
 
 contract('ExpertyToken', accounts => {
   let token;
   it("should initialize experty token", async () => {
-    //TODO: test other parameters of constructor.
     token = await ExpertyToken.new();
     const contractManager = await token.contractManager.call();
-    assert.equal(accounts[0], contractManager, 'Contract manager not equal to first account')
+    assert.equal(accounts[0], contractManager, 'Contract manager not equal to first account');
     const name = await token.name.call();
-    assert.equal('Experty Token', name, 'Contract name is not equal to ExpertyToken')
+    assert.equal('Experty Token', name, 'Contract name is not equal to ExpertyToken');
     const symbol = await token.symbol.call();
-    assert.equal('EXY', symbol, 'Contract symbol is not equal to EXY')
+    assert.equal('EXY', symbol, 'Contract symbol is not equal to EXY');
     const decimals = await token.decimals.call();
-    assert.equal(18, decimals, 'Contract decimals is not equal to 18')
+    assert.equal(18, decimals, 'Contract decimals is not equal to 18');
     const totalSupply = await token.totalSupply.call();
-    assert.equal(0, totalSupply, 'Contract totalSupply is not equal to 0')
+    assert.equal(0, totalSupply, 'Contract totalSupply is not equal to 0');
     const circulatingSupply = await token.circulatingSupply.call();
-    assert.equal(0, circulatingSupply, 'Contract circulatingSupply is not equal to 0')
+    assert.equal(0, circulatingSupply, 'Contract circulatingSupply is not equal to 0');
   });
   it("should add contribution, total supply increasing by 1000", async () => {
     const initialSupply = await token.totalSupply.call();
@@ -37,9 +47,20 @@ contract('ExpertyToken', accounts => {
   });
   it("should increase total supply after add partner allocation", async () => {
     const initialSupply = await token.totalSupply.call();
+    // add partners contribution
+    // address (accounts[0]),
+    // with 200 exy per period,
+    // 10 periods
+    // and 1 month period duration
     await token.addPartnersAllocation.sendTransaction(accounts[2], 200, 10, 1);
     const supplyAfterFirstAllocation = await token.totalSupply.call();
     assert(supplyAfterFirstAllocation.eq(initialSupply.add(2000)), 'Total supply not increased');
+    const lockedContrubution = await token.lockedContributions.call(accounts[2]);
+    assert(lockedContrubution[EXY_PER_PERIOD_FIELD].eq(200), 'Experty tokens per period has not been set');
+    assert(lockedContrubution[PERIOD_FIELD].eq(10), 'Periods has not been set');
+    // claimed periods are default set to 0
+    assert(lockedContrubution[CLAIMED_PERIODS_FIELD].eq(0), 'Claimed periods has not been set');
+    assert(lockedContrubution[PERIOD_DURAITON_FIELD].eq(1), 'Period duration has not been set');
   })
 });
 
