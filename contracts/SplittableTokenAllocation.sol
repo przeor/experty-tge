253d46183d06b9aa657e33ff9df5504a16d442a8
hstemplewski <huberts@reactpoland.com>
2018-01-19 17:49:51 +0100

    Add splittableTokenAllocation constructor

--- contracts/SplittableTokenAllocation.sol
+++ contracts/SplittableTokenAllocation.sol
@@ -3,15 +3,25 @@ pragma solidity ^0.4.4;
 
 contract SplittableTokenAllocation {
 
-	struct SplitT {
-	  address source;
-	  address dest;
-	  uint tokens;
-	  bool isApproved;
-	  bool isExecuted;
-	}
+  struct SplitT {
+    address source;
+    address dest;
+    uint tokens;
+    bool isApproved;
+    bool isExecuted;
+  }
+
+  uint public exyPerPeriod;
+  uint public periods;
+  uint public months;
 
   SplitT[] public splits;
+
+  function SplittableTokenAllocation(uint _exyPerPeriod, uint _periods, uint _months) {
+    exyPerPeriod = _exyPerPeriod;
+    periods = _periods;
+    months = _months;
+  }
 
   function proposeSplit(address splitPool, address dest, uint tokens) public returns(uint) {
     splits.push(SplitT({
@@ -26,7 +36,7 @@ contract SplittableTokenAllocation {
   function getLastSplitId() public view returns (uint) {
     return splits.length - 1;
   }
-  
+
   function approveSplit(uint splitId) public {
     splits[splitId].isApproved = true;
   }
--- test/TestSplittableToken.js
+++ test/TestSplittableToken.js
@@ -14,11 +14,28 @@ const TOKENS_FIELD = 2;
 const IS_APPROVED_FIELD = 3;
 const IS_EXECUTED_FIELD = 4;
 
+// Constructor params
+// uint exyPerPeriod;
+// uint periods;
+// uint months;
+const MOCK_EXPERTY_PER_PERIOD = 1000;
+const MOCK_PERIODS = 10;
+const MOCK_MONTHS = 2;
+
 contract('SplittableTokenAllocation', accounts => {
   let splittableTokenAllocation;
 
+  it('should initialise splittable allocation token', async () => {
+    splittableTokenAllocation = await SplittableTokenAllocation.new(MOCK_EXPERTY_PER_PERIOD, MOCK_PERIODS, MOCK_MONTHS);
+    const exyPerPeriod = await splittableTokenAllocation.exyPerPeriod.call();
+    assert.equal(exyPerPeriod, MOCK_EXPERTY_PER_PERIOD, 'Experty token per period has not been set correctly');
+    const periods = await splittableTokenAllocation.periods.call();
+    assert.equal(periods, MOCK_PERIODS, 'Period has not been set correctly');
+    const months = await splittableTokenAllocation.months.call();
+    assert.equal(months, MOCK_MONTHS, 'Months has not been set correctly');
+  });
+
   it('should propose and accept split allocation', async () => {
-    splittableTokenAllocation = await SplittableTokenAllocation.new();
     const splitPool = 0x0;
     const destAddr = accounts[0];
     await splittableTokenAllocation.proposeSplit.sendTransaction(splitPool, destAddr, 100);
