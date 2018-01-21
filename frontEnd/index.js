df90349db07d4916f66763aea62ba7390416fa79
hstemplewski <huberts@reactpoland.com>
2018-01-21 16:48:34 +0100

    Prepare tables with mocked data

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,8 +1,9 @@
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-abi = require('../build/contracts/SplittableTokenAllocation.json');
-console.log('ABI ', abi);
-SplittableTokenAllocation = web3.eth.contract(abi);
-splittableTokenInstance = SplittableTokenAllocation.at('address here');
+const Web3 = require('web3');
+const ABI = '';
+const PROVIDER = '';
+const ADDRESS = '';
+// const web3 = new Web3(new Web3.providers.HttpProvider(HTTP_PROVIDER));
+// splittableTokenInstance = new web3.eth.Contract(ABI, ADDRESS);
 
 const SplitTFields = {
   tokensPerField: 0,
@@ -11,17 +12,67 @@ const SplitTFields = {
   state: 3
 }
 
-const splits = splittableTokenInstance.splitOf.call();
+const mockSplitElement = {
+  address: '0x9b4f4e32c9c825b035f127ddcd086e06ddca3f69',
+  state: 'Appropved',
+  claimedPeriods: 1,
+  tokensPerPeriod: 100
+}
 
-const tbody = document.getElementById('tbody');
-splits.map(split => {
-  /* TODO return structure:
-  *  <tr>
-  *    <td>Address here</td>
-  *    <td>Value here</td>
-  *    <td>State here</td>
-  *  </tr>
-  */
+const mockCompanyAllocationResponse = [
+  mockSplitElement,
+  mockSplitElement
+];
 
-  tbody.appendChild(/* above structure hare */);
-});
+const mockPartnerAllocationResponse = [
+  mockSplitElement,
+  mockSplitElement
+];
+
+const mockBountyElement = {
+  address: '0x9b4f4e32c9c825b035f127ddcd086e06ddca3f69',
+  state: 'Appropved',
+  tokens: 100
+}
+
+const mockBountyResponse = [
+  mockBountyElement,
+  mockBountyElement
+];
+
+function addCell(value, tr) {
+  const cell = document.createElement('td');
+  cell.innerHTML = value;
+  tr.appendChild(cell);
+}
+
+function fullFillSplittableTable(tableId, mockResponse) {
+  const table = document.getElementById(tableId);
+  mockResponse.map(res => {
+    const tr = document.createElement('tr');
+
+    addCell(res.address, tr);
+    addCell(res.tokensPerPeriod, tr);
+    addCell(res.claimedPeriods, tr);
+    addCell(res.state, tr);
+
+    table.appendChild(tr);
+  });
+}
+function fullFillBountyTable(tableId, mockResponse) {
+  const table = document.getElementById(tableId);
+  mockResponse.map(res => {
+    const tr = document.createElement('tr');
+
+    addCell(res.address, tr);
+    addCell(res.tokens, tr);
+    addCell(res.state, tr);
+
+    table.appendChild(tr);
+  });
+}
+fullFillSplittableTable('tkn-company-allocation-tbody', mockCompanyAllocationResponse);
+fullFillSplittableTable('tkn-partner-allocation-tbody', mockPartnerAllocationResponse);
+fullFillBountyTable('tkn-bounty-allocation-tbody', mockBountyResponse);
+
+
