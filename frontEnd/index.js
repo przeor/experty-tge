b0580da6ca96c0b9e2445d9d15b3ec66d1ea6a9a
albertbalinski <albert.balinski@gmail.com>
2018-01-22 16:57:09 +0100

    Add deployment of contracts in js

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -59,53 +59,67 @@ function fullFillBountyTable(tableId, mockResponse) {
 const Web3 = require('web3');
 
 fetch('contracts/ExyToken.json')
-.then(res => res.json())
-.then(res => {
-  const ABI = res.abi;
-  const PROVIDER = 'http://localhost:8545';
-  const ADDRESS = '0x5e2c81cbb9904ff4d632fdc098fb3ef46f79a9ad';
-  if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER));
-  }
-  web3.eth.defaultAccount = web3.eth.accounts[0];
-  const ExyTokenContract = web3.eth.contract(ABI);
-  const exyTokenInstance = ExyTokenContract.at(ADDRESS);
+  .then(res => res.json())
+  .then(res => {
+    const ABI = res.abi;
+    const BYTECODE = res.bytecode;
+    const PROVIDER = 'http://localhost:8545';
+    const ADDRESS = '0xcae968005d178839204bdf0ee19ae1afbeccd8f7';
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER));
+    }
+    console.log(web3);
+    web3.eth.defaultAccount = web3.eth.accounts[0];
+    const ExyTokenContract = web3.eth.contract(ABI);
 
-  function getAndFullfillSplitable(lengthGetter, allocationGetter, tableId, fulfillFunction) {
-    lengthGetter.call((err,res) => {
-      const companyListLength = res.toNumber();
-      const promises = [];
-      function createPromise(id) {
-        return new Promise(function (resolve, reject) {
-          allocationGetter.call(id, (err, res) => {
-            resolve(res);
-          })
+    var DEPLOY = true;
+    if (DEPLOY) {
+      ExyTokenContract.new({
+        data: '0x' + BYTECODE,
+        from: web3.eth.coinbase,
+        gas: 90000 * 2
+      }, function (err, res) {
+        console.log(err);
+        console.log(res);
+      })
+    } else {
+      const exyTokenInstance = ExyTokenContract.at(ADDRESS);
+
+      function getAndFullfillSplitable(lengthGetter, allocationGetter, tableId, fulfillFunction) {
+        lengthGetter.call((err, res) => {
+          const companyListLength = res.toNumber();
+          const promises = [];
+          function createPromise(id) {
+            return new Promise(function (resolve, reject) {
+              allocationGetter.call(id, (err, res) => {
+                resolve(res);
+              })
+            });
+          }
+          for (let i = 0; i < companyListLength; ++i) {
+            promises.push(createPromise(i));
+          }
+          Promise.all(promises).then(res =>
+            fulfillFunction(tableId, res)
+          );
         });
       }
-      for (let i = 0; i < companyListLength; ++i) {
-        promises.push(createPromise(i));
-      }
-      Promise.all(promises).then(res =>
-        fulfillFunction(tableId, res)
-      );
-    });
-  }
-
-  getAndFullfillSplitable(
-    exyTokenInstance.getCompanyAllocationListLength,
-    exyTokenInstance.getCompanyAllocation,
-    'tkn-company-allocation-tbody',
-    fullFillSplittableTable);
-  getAndFullfillSplitable(
-    exyTokenInstance.getPartnerAllocationListLength,
-    exyTokenInstance.getPartnerAllocation,
-    'tkn-partner-allocation-tbody',
-    fullFillSplittableTable);
-  getAndFullfillSplitable(
-    exyTokenInstance.getBountyAllocationListLength,
-    exyTokenInstance.getBountyAllocation,
-    'tkn-bounty-allocation-tbody',
-    fullFillBountyTable);
-})
+      getAndFullfillSplitable(
+        exyTokenInstance.getCompanyAllocationListLength,
+        exyTokenInstance.getCompanyAllocation,
+        'tkn-company-allocation-tbody',
+        fullFillSplittableTable);
+      getAndFullfillSplitable(
+        exyTokenInstance.getPartnerAllocationListLength,
+        exyTokenInstance.getPartnerAllocation,
+        'tkn-partner-allocation-tbody',
+        fullFillSplittableTable);
+      getAndFullfillSplitable(
+        exyTokenInstance.getBountyAllocationListLength,
+        exyTokenInstance.getBountyAllocation,
+        'tkn-bounty-allocation-tbody',
+        fullFillBountyTable);
+    }
+  });
